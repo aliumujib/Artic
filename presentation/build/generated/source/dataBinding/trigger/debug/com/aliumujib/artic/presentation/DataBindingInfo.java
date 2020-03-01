ed864a5095037f350c4a98d3cf0a03ef7428af76
aliumujib <aliuabdulmujib@yahoo.com>
2020-03-01 14:02:38 +0100

    Deleted presnation module, moved viewmodels to dynamic feature modules

--- presentation/build/generated/source/dataBinding/trigger/debug/com/aliumujib/artic/presentation/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.aliumujib.artic.presentation;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- presentation/build/generated/source/kapt/debug/com/aliumujib/artic/presentation/BR.java
+++
@@ -1,5 +0,0 @@
-package com.aliumujib.artic.presentation;
-
-public class BR {
-  public static int _all = 0;
-}
