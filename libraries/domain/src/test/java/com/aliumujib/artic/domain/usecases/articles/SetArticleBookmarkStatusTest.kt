e0b62c13ac4d49a420bcc67c40e8b0b575e7ad23
aliumujib <aliuabdulmujib@yahoo.com>
2020-03-21 02:19:46 +0100

    Fixed some broken tests

--- libraries/domain/src/test/java/com/aliumujib/artic/domain/usecases/articles/SetArticleBookmarkStatusTest.kt
+++ libraries/domain/src/test/java/com/aliumujib/artic/domain/usecases/articles/SetArticleBookmarkStatusTest.kt
@@ -1,7 +1,9 @@
 package com.aliumujib.artic.domain.usecases.articles
 
+import com.aliumujib.artic.domain.models.Article
 import com.aliumujib.artic.domain.threadexecutor.PostExecutionThread
 import com.aliumujib.artic.domain.repositories.articles.IArticlesRepository
+import com.aliumujib.artic.domain.test.ArticleDataFactory
 import com.aliumujib.artic.domain.test.TestPostExecutionThreadImpl
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
@@ -30,25 +32,41 @@ class SetArticleBookmarkStatusTest {
 
 
     @Test
-    fun `test that calling bookmarkArticle completes`() = runBlockingTest {
-            stubBookmarkOperation()
-            val result = setArticleBookmarkStatus.invoke(SetArticleBookmarkStatus.Params(randomBuild()))
-            MatcherAssert.assertThat(result, `is`((instanceOf(Unit::class.java))))
+    fun `test that calling bookmarkArticle with false completes`() = runBlockingTest {
+        val article = ArticleDataFactory.makeProject()
+        stubBookmarkOperation(article)
+        val result = setArticleBookmarkStatus.invoke(SetArticleBookmarkStatus.Params(article, false))
+        MatcherAssert.assertThat(result, `is`((instanceOf(Article::class.java))))
+    }
+
+
+    @Test
+    fun `test that calling bookmarkArticle with true completes`() = runBlockingTest {
+        val article = ArticleDataFactory.makeProject()
+        stubUnBookmarkOperation(article)
+        val result = setArticleBookmarkStatus.invoke(SetArticleBookmarkStatus.Params(article, true))
+        MatcherAssert.assertThat(result, `is`((instanceOf(Article::class.java))))
     }
 
 
     @Test(expected = IllegalStateException::class)
     fun `test that calling bookmarkArticle without params throws an exception`() = runBlockingTest {
-        stubBookmarkOperation()
+        stubBookmarkOperation(randomBuild())
         val result = setArticleBookmarkStatus.invoke()
         MatcherAssert.assertThat(result, `is`((instanceOf(IllegalStateException::class.java))))
     }
 
 
-    private fun stubBookmarkOperation() {
+    private fun stubUnBookmarkOperation(article: Article) {
         coEvery {
-            articlesRepository.bookmarkArticle(any())
-        } returns Unit
+            articlesRepository.unBookmarkArticle(article.id)
+        } returns article
+    }
+
+    private fun stubBookmarkOperation(article: Article) {
+        coEvery {
+            articlesRepository.bookmarkArticle(article)
+        } returns article
     }
 
 }
